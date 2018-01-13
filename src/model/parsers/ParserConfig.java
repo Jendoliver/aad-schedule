5f238bb45a58d40c0e8db50876f572e24201a651
DESKTOP-GGMSK8T\magne <javierpuiggener@gmail.com>
2018-01-13 20:19:50 +0100

    ParserInternational+ParserConfig Implemented

--- src/model/parsers/ParserConfig.java
+++ src/model/parsers/ParserConfig.java
@@ -1,5 +1,11 @@
 package model.parsers;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import globals.Configuration;
+
 /**
  * The class responsible for parsing the configuration file and setting the globals.Configuration values.
  * 
@@ -13,9 +19,38 @@ public class ParserConfig extends Parser
 		this.fileToParse = fileToParse;
 	}
 
-	public void parse() 
+	public void parse()  // Parse fileToParse and fill Configuration
 	{
-		// TODO Auto-generated method stub
+		String[] configEntry;
+		try 
+		{
+			BufferedReader br = new BufferedReader(new FileReader(fileToParse));
+			String sCurrentLine;
+			while ((sCurrentLine = br.readLine()) != null) 
+			{
+				configEntry = sCurrentLine.split(";");
+				switch(configEntry[0]) 
+				{
+					case "001": //001 -> Year code
+						Configuration.YEAR_TO_PROCESS = configEntry[1];
+						break;
+					case "002": //002 -> Month code
+						Configuration.MONTH_TO_PROCESS = configEntry[1];
+						break;
+					case "003": //003 -> Input code
+						Configuration.INPUT_LANG = configEntry[1];
+						break;
+					case "004": //004 -> Output code
+						Configuration.OUTPUT_LANG = configEntry[1];
+						break;
+					default:
+						break;
+				}
+				br.close();
+            }
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
 	}
 
 }
--- src/model/parsers/ParserInternational.java
+++ src/model/parsers/ParserInternational.java
@@ -1,5 +1,12 @@
 package model.parsers;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import globals.InputStrings;
+import globals.OutputStrings;
+
 /**
  * The class responsible for parsing the international files and setting the globals.InputStrings and globals.OutputStrings values
  * 
@@ -17,14 +24,83 @@ public class ParserInternational extends Parser
 
 	public void parse() 
 	{
-		if(isInput)
+		String[] parameters;
+		if(isInput) // Parse fileToParse and fill InputStrings (003,005)
 		{
-			// TODO parse fileToParse and fill InputStrings (003)
+			try 
+			{
+				BufferedReader br = new BufferedReader(new FileReader(fileToParse));
+				String sCurrentLine;
+				while ((sCurrentLine = br.readLine()) != null) 
+				{
+					String[] configEntry = sCurrentLine.split(";");
+					if(configEntry[0].equalsIgnoreCase("003")) //003 -> Days code
+					{
+		                parameters = configEntry[1].split(",");
+		                InputStrings.MONDAY = parameters[0];
+		                InputStrings.TUESDAY = parameters[1]; 
+		                InputStrings.WEDNESDAY = parameters[2]; 
+		                InputStrings.THURSDAY = parameters[3]; 
+		                InputStrings.FRIDAY = parameters[4]; 
+		                InputStrings.SATURDAY = parameters[5]; 
+		                InputStrings.SUNDAY = parameters[6]; 
+					}
+					else if(configEntry[0].equalsIgnoreCase("005")) //005 -> Close code
+					{
+						InputStrings.CLOSE_KEY = configEntry[1];
+					}
+					br.close();
+	            }
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
 		}
 		
-		else
+		else // Parse fileToParse and fill OutputStrings(001, 002, 004)
 		{
-			// TODO parse fileToParse and fill OutputStrings(001, 002, 004, 005)
+			try 
+			{
+				BufferedReader br = new BufferedReader(new FileReader(fileToParse));
+				String sCurrentLine;
+				while ((sCurrentLine = br.readLine()) != null) 
+				{
+					String[] configEntry = sCurrentLine.split(";");
+					if(configEntry[0].equalsIgnoreCase("001")) // 001 -> Title Code
+					{
+		                OutputStrings.TITLE = configEntry[1];
+					}
+					else if(configEntry[0].equalsIgnoreCase("002")) // 002 -> Days (full) code
+					{
+						parameters = configEntry[1].split(",");
+						OutputStrings.Days.MONDAY=parameters[0];
+						OutputStrings.Days.TUESDAY=parameters[1]; 
+						OutputStrings.Days.WEDNESDAY=parameters[2]; 
+						OutputStrings.Days.THURSDAY=parameters[3]; 
+						OutputStrings.Days.FRIDAY=parameters[4]; 
+						OutputStrings.Days.SATURDAY=parameters[5]; 
+						OutputStrings.Days.SUNDAY=parameters[6]; 
+					}
+					else if(configEntry[0].equalsIgnoreCase("004")) // 004 -> Month code
+					{
+						parameters = configEntry[1].split(",");
+						OutputStrings.Months.JANUARY=parameters[0];
+						OutputStrings.Months.FEBRUARY=parameters[1];
+						OutputStrings.Months.MARCH=parameters[2];
+						OutputStrings.Months.APRIL=parameters[3];
+						OutputStrings.Months.MAY=parameters[4];
+						OutputStrings.Months.JUNE=parameters[5];
+						OutputStrings.Months.JULY=parameters[6];
+						OutputStrings.Months.AUGUST=parameters[7];
+						OutputStrings.Months.SEPTEMBER=parameters[8];
+						OutputStrings.Months.OCTOBER=parameters[9];
+						OutputStrings.Months.NOVEMBER=parameters[10];
+						OutputStrings.Months.DECEMBER=parameters[11];
+					}
+	            }
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
