086e91842fdb6b93f1d6f4761c9c4d616bf20502
Irene <icabezasgomez@gmail.com>
2018-01-18 13:19:39 +0100

    Changed method to enum.

--- src/main/globals/Constants.java
+++ src/main/globals/Constants.java
@@ -10,10 +10,16 @@ public final class Constants
 {
 	public static final int MAX_HOUR_FRAME_NUM = 5;
 	
-	public final class FileNames 
+	public enum FileNames 
 	{
-		public static final String CONFIG = "config.txt";
-		public static final String INTERNATIONAL_PRE = "international."; // This doesn't have an extension because it depends on the configuration file.
-		public static final String REQUESTS = "requests.txt";
+		CONFIG("config.txt"), INTERNATIONAL_PRE("international."), REQUESTS("requests.txt"); // This doesn't have an extension because it depends on the configuration file.
+
+		private String name;
+		private FileNames(String name) { this.name = name; }
+
+		/**
+		 * Returns the actual name of the file as a String
+		 */
+		public String getName() { return name; }
 	}
 }
